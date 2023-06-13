279542552de940c633cee2b1d70a7699d4f348f8
jpts <james@cleverley-prance.uk>
2023-06-13 15:17:33 +0100

    feat: add rbac-police

---
+++ pkgs/rbac-police/default.nix
@@ -0,0 +1,36 @@
+{
+  stdenv,
+  lib,
+  pkgs,
+}:
+
+pkgs.buildGoModule rec {
+  pname = "rbac-police";
+  version = "1.1.2";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "PaloAltoNetworks";
+    repo = "rbac-police";
+    rev = "v${version}";
+    sha256 = "sha256-TbjN+xH3DIfzwxA7p9NCvWkvz4bEUPilsq685DgiV1k=";
+  };
+
+  vendorSha256 = "sha256-oTvXlJpcplT6W5XMMuC9oNkYaTku1MHGRVE9eLz4H3M=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+  meta = with lib; {
+    homepage = "https://github.com/PaloAltoNetworks/rbac-police";
+    changelog = "https://github.com/PaloAltoNetworks/rbac-police/releases/tag/v${version}";
+    description = "Evaluate the RBAC permissions of Kubernetes identities through policies written in Rego";
+    longDescription = ''
+      Retrieve the RBAC permissions of Kubernetes identities - service accounts, pods, nodes, users and groups - and evaluate them using policies written in Rego.
+      The policy library includes over 20 policies that detect identities possessing risky permissions, each alerting on a different attack path.
+    '';
+    license = licenses.mit;
+    maintainers = with maintainers; [];
+    platforms = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+  };
+}
