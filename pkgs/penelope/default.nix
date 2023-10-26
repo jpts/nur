b6835a569082f3b88ab28fda6868b59ab57f796b
jpts <james@cleverley-prance.uk>
2023-10-27 00:28:40 +0100

    feat: bump penelope to latest commit

--- pkgs/penelope/default.nix
+++ pkgs/penelope/default.nix
@@ -1,46 +1,57 @@
-{
-  lib,
-  stdenv,
-  buildPythonPackage,
-  fetchFromGitHub,
+{ lib
+, stdenv
+, buildPythonPackage
+, fetchFromGitHub
+, fetchpatch
+, setuptools
+, python3
 }:
 buildPythonPackage rec {
   name = "penelope";
-  version = "0.9.2";
   owner = "brightio";
+  version = "0.9.2.post0";
+  commit = "81fd31339c697fee5223a9a6e16bd5d26199019f";
   repo = name;
+  format = "pyproject";
 
   src = fetchFromGitHub {
     inherit owner repo;
-    rev = "v${version}";
-    sha256 = "sha256-KlbB7i009UttTp+/MaF3y9CGUkAGEQQq/W+hQYKk9CY=";
+    rev = "81fd31339c697fee5223a9a6e16bd5d26199019f";
+    sha256 = "sha256-AQXIJQ8UEq81a+VUXDG/F/oVEC8vIU/bCdSsBLrLsac=";
   };
 
-  preBuild = ''
-    cat > setup.py << EOF
-    from setuptools import setup
+  patches = [
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/brightio/penelope/pull/17.patch";
+      sha256 = "sha256-nlhqQrmih7n4cDNqCqOJn3KJLnmKvd8HfUDk7+m8xLo=";
+    })
+  ];
 
-    setup(
-      name='penelope',
-      version='${version}',
-      scripts=[
-        'penelope.py',
-      ],
-    )
-    EOF
+  nativeBuildInputs = with python3.pkgs; [
+    setuptools
+    wheel
+  ];
+
+  postPatch = ''
+    sed -i -e 's/^  version = .*/  version = "${version}"/g' pyproject.toml
   '';
 
-  installPhase = "install -Dm755 $src/penelope.py $out/bin/penelope";
+  # We are a script, no tests
+  doCheck = false;
+
+  postInstall = ''
+    mv $out/bin/penelope.py $out/bin/penelope
+  '';
 
   meta = {
     homepage = "https://github.com/brightio/penelope";
     changelog = "https://github.com/brightio/penelope/tag/v${version}";
     description = "Reverse Shell Handler";
     longDescription = ''
-      Penelope is a shell handler designed to be easy to use and intended to replace netcat when exploiting RCE vulnerabilities. It is compatible with Linux and macOS and requires Python 3.6 or higher. It is a standalone script that does not require any installation or external dependencies, and it is intended to remain this way.
+      Penelope is a shell handler designed to be easy to use and intended to replace netcat when exploiting RCE vulnerabilities.
     '';
-    license = lib.licenses.gpl3;
-    maintainers = with lib.maintainers; [];
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ jpts ];
     platforms = lib.platforms.unix;
   };
 }
