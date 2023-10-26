6c6fe25e4e730d55713b84efc7c3bf9af253d72d
jpts <james@cleverley-prance.uk>
2023-10-27 00:37:46 +0100

    feat: add scoutsuite poc

--- default.nix
+++ default.nix
@@ -5,26 +5,34 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{ pkgs ? import <nixpkgs> { } }: {
-  # The `lib`, `modules`, and `overlay` names are special
-  #lib = import ./lib {inherit pkgs;}; # functions
-  #modules = import ./modules; # NixOS modules
-  #overlays = import ./overlays; # nixpkgs overlays
+{ pkgs ? import <nixpkgs> { } }:
+let
+  nurPkgs = rec {
+    # The `lib`, `modules`, and `overlay` names are special
+    lib = import ./lib { inherit pkgs; }; # functions
+    #modules = import ./modules; # NixOS modules
+    #overlays = import ./overlays; # nixpkgs overlays
 
-  rbac-police = pkgs.callPackage ./pkgs/rbac-police { };
-  coredns-enum = pkgs.callPackage ./pkgs/coredns-enum { };
-  penelope = pkgs.python3Packages.callPackage ./pkgs/penelope { };
-  kubectl-execws = pkgs.callPackage ./pkgs/kubectl-execws { };
-  stats = pkgs.callPackage ./pkgs/stats { };
-  blueutil = pkgs.callPackage ./pkgs/blueutil {
-    inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation IOBluetooth;
+    rbac-police = pkgs.callPackage ./pkgs/rbac-police { };
+    coredns-enum = pkgs.callPackage ./pkgs/coredns-enum { };
+    penelope = pkgs.python3Packages.callPackage ./pkgs/penelope { };
+    kubectl-execws = pkgs.callPackage ./pkgs/kubectl-execws { };
+    stats = pkgs.callPackage ./pkgs/stats { };
+    blueutil = pkgs.callPackage ./pkgs/blueutil {
+      inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation IOBluetooth;
+    };
+    sleepwatcher = pkgs.callPackage ./pkgs/sleepwatcher {
+      inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation IOKit;
+    };
+    patator = pkgs.python3Packages.callPackage ./pkgs/patator { };
+    cherrypy-cors = pkgs.python3Packages.callPackage ./pkgs/cherrypy-cors { };
+    httplib2shim = pkgs.python3Packages.callPackage ./pkgs/httplib2shim { };
+    insomnia = pkgs.callPackage ./pkgs/insomnia { };
+    inso = pkgs.callPackage ./pkgs/inso { };
+    scoutsuite = pkgs.python3Packages.callPackage ./pkgs/scoutsuite {
+      cherrypy-cors = nurPkgs.cherrypy-cors;
+      httplib2shim = nurPkgs.httplib2shim;
+    };
   };
-  sleepwatcher = pkgs.callPackage ./pkgs/sleepwatcher {
-    inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Foundation IOKit;
-  };
-  patator = pkgs.python3Packages.callPackage ./pkgs/patator { };
-  cherrypy-cors = pkgs.python3Packages.callPackage ./pkgs/cherrypy-cors { };
-  httplib2shim = pkgs.python3Packages.callPackage ./pkgs/httplib2shim { };
-  insomnia = pkgs.callPackage ./pkgs/insomnia {};
-  inso = pkgs.callPackage ./pkgs/inso {};
-}
+in
+nurPkgs
