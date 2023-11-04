5f8611a6a8fb36831aa0d5a27147e073422dc9e3
jpts <james@cleverley-prance.uk>
2023-11-04 10:14:14 +0000

    feat: update penelope

--- pkgs/penelope/default.nix
+++ pkgs/penelope/default.nix
@@ -9,39 +9,27 @@
 buildPythonPackage rec {
   name = "penelope";
   owner = "brightio";
-  version = "0.9.2.post0";
-  commit = "81fd31339c697fee5223a9a6e16bd5d26199019f";
+  version = "0.10.0";
+  #owner = "jpts";
+  commit = "83a68680ca91fe813af5cf3a2006c96c15cb070f";
   repo = name;
   format = "pyproject";
 
   src = fetchFromGitHub {
     inherit owner repo;
-    rev = "81fd31339c697fee5223a9a6e16bd5d26199019f";
-    sha256 = "sha256-AQXIJQ8UEq81a+VUXDG/F/oVEC8vIU/bCdSsBLrLsac=";
+    #rev = "refs/tags/v${version}";
+    rev = commit;
+    sha256 = "sha256-r8ZMwPAKLITHDDafiXb/fE+H8sJnQUgUtbjwQbMko7E=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/brightio/penelope/pull/17.patch";
-      sha256 = "sha256-nlhqQrmih7n4cDNqCqOJn3KJLnmKvd8HfUDk7+m8xLo=";
-    })
-  ];
 
   nativeBuildInputs = with python3.pkgs; [
     setuptools
     wheel
   ];
 
-  postPatch = ''
-    sed -i -e 's/^  version = .*/  version = "${version}"/g' pyproject.toml
-  '';
-
   # We are a script, no tests
   doCheck = false;
-
-  postInstall = ''
-    mv $out/bin/penelope.py $out/bin/penelope
-  '';
+  pythonImportsCheck = [ "penelope" ];
 
   meta = {
     homepage = "https://github.com/brightio/penelope";
