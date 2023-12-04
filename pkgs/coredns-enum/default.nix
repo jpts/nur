7f60c2111ea508ea4f15a769ef850175988e1717
jpts <james@cleverley-prance.uk>
2023-12-04 23:06:16 +0000

    nit: sha256 -> hash

--- pkgs/coredns-enum/default.nix
+++ pkgs/coredns-enum/default.nix
@@ -1,8 +1,7 @@
-{
-  lib,
-  stdenv,
-  buildGoModule,
-  fetchFromGitHub,
+{ lib
+, stdenv
+, buildGoModule
+, fetchFromGitHub
 }:
 buildGoModule rec {
   pname = "coredns-enum";
@@ -13,9 +12,9 @@ buildGoModule rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "v${version}";
-    sha256 = "sha256-EITgW6+gVLkcLqc368iw3wYBy5fXXjUC2VTMYBSv2pw=";
+    hash = "sha256-EITgW6+gVLkcLqc368iw3wYBy5fXXjUC2VTMYBSv2pw=";
   };
-  vendorSha256 = "sha256-JyOb+Apnw+lmeHTb0X32GhNObEyn31L8mFNNLtQxxI8=";
+  vendorHash = "sha256-JyOb+Apnw+lmeHTb0X32GhNObEyn31L8mFNNLtQxxI8=";
 
   ldflags = [
     "-s"
@@ -32,7 +31,7 @@ buildGoModule rec {
       The tool has two modes: wildcard & bruteforce. It will automagically detect if the DNS server you are targeting supports CoreDNS wildcards (< v1.9.0) and fallback to the bruteforce method if not. The bruteforce mode also tries to guess sensible CIDR ranges to scan by default (through parsing the API server HTTPS certificate). You can override this.
     '';
     license = licenses.mit;
-    maintainers = with maintainers; [];
-    platforms = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    maintainers = with maintainers; [ ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
   };
 }
